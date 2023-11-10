f1ccca9e664e9a02bd5f9506ed3f84b495573e8e
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-11-10 21:11:50 +0100

    Update the run-all script

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export LC_NUMERIC="en_US.UTF-8"
+
 ROOT=`pwd`
 JULIET=$ROOT/clog-programs/juliet
 
@@ -11,12 +13,14 @@ declare -a CONFIGS=("uncontrolled_format_string.json"
                     "heap_buffer_overflow.json"
                     "buffer_overread.json"
                     "buffer_underread.json"
+                    "buffer_underwrite.json"
                     "write_what_where.json")
 
 collect_results() {
     local config=$1
     local file=$2
-    local name=`basename $config`
+    # local name=`basename $config`
+    local name=`jq -r ".manifestFilter" $config | sed 's|\.\*||g' | sed 's|CWE|CWE-|g'`
     local output_dir=`jq -r ".outputDir" $config`
     local clang_args=`jq -r ".clangXargs[-1]" $config | sed 's|--checks=-clang-analyzer-\*,||g' | sed 's|--checks=-clang-analyzer\*,||g' | sed 's|clang-analyzer-||g'`
     local n_ground=`grep -c "^.*" $output_dir/ground.csv`
@@ -24,14 +28,16 @@ collect_results() {
     local n_false_positive_clang=`grep -c "^.*" $output_dir/clang.false.positive.csv`
     local n_true_positive_clog=`grep -c "^.*" $output_dir/clog.true.positive.csv`
     local n_false_positive_clog=`grep -c "^.*" $output_dir/clog.false.positive.csv`
-    local clang_precision=`bc <<< "scale=2; $n_true_positive_clang/($n_true_positive_clang + $n_false_positive_clang) * 100"`
-    local clang_recall=`bc <<< "scale=2; $n_true_positive_clang/$n_ground * 100"`
-    local clog_precision=`bc <<< "scale=2; $n_true_positive_clog/($n_true_positive_clog + $n_false_positive_clog) * 100"`
-    local clog_recall=`bc <<< "scale=2; $n_true_positive_clog/$n_ground * 100"`
+    local clang_precision=`bc <<< "scale=0; $n_true_positive_clang/($n_true_positive_clang + $n_false_positive_clang) * 100"`
+    local clang_recall=`bc <<< "scale=0; $n_true_positive_clang/$n_ground * 100"`
+    local clog_precision=`bc <<< "scale=0; $n_true_positive_clog/($n_true_positive_clog + $n_false_positive_clog) * 100"`
+    local clog_recall=`bc <<< "scale=0; $n_true_positive_clog/$n_ground * 100"`
     local clang_time=`cat $output_dir/clang.analysis.time`
+    local clang_time_f=`printf "%.2f" $clang_time`
     local clog_time=`cat $output_dir/clog.analysis.time`
+    local clog_time_f=`printf "%.2f" $clog_time`
 
-    echo "$name & $clang_args & $n_ground & $n_true_positive_clang & $n_true_positive_clog & $clang_recall & $clog_recall & $n_false_positive_clang & $n_false_positive_clog & $clang_precision & $clog_precision & $clang_time & $clog_time \\tabularnewline" >> $file
+    echo "$name & $clang_args & $n_ground & $n_true_positive_clang & $n_true_positive_clog & $clang_recall & $clog_recall & $n_false_positive_clang & $n_false_positive_clog & $clang_precision & $clog_precision & $clang_time_f & $clog_time_f \\tabularnewline" >> $file
 }
 
 RESULT_FILE=juliet-results.tex
@@ -39,6 +45,6 @@ RESULT_FILE=juliet-results.tex
 echo "" > $RESULT_FILE
 
 for config in "${CONFIGS[@]}"; do
-    ./run.sh $JULIET/$config
+    # ./run.sh $JULIET/$config
     collect_results $JULIET/$config $RESULT_FILE
 done
