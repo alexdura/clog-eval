59d0073e4c39830171eb093361c9544da3a0c0f6
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-11-11 11:06:11 +0100

    Update the run-all script

--- run-all.sh
+++ run-all.sh
@@ -19,7 +19,7 @@ declare -a CONFIGS=("uncontrolled_format_string.json"
 collect_results() {
     local config=$1
     local file=$2
-    # local name=`basename $config`
+    local config_name=`basename $config`
     local name=`jq -r ".manifestFilter" $config | sed 's|\.\*||g' | sed 's|CWE|CWE-|g'`
     local output_dir=`jq -r ".outputDir" $config`
     local clang_args=`jq -r ".clangXargs[-1]" $config | sed 's|--checks=-clang-analyzer-\*,||g' | sed 's|--checks=-clang-analyzer\*,||g' | sed 's|clang-analyzer-||g'`
@@ -28,16 +28,17 @@ collect_results() {
     local n_false_positive_clang=`grep -c "^.*" $output_dir/clang.false.positive.csv`
     local n_true_positive_clog=`grep -c "^.*" $output_dir/clog.true.positive.csv`
     local n_false_positive_clog=`grep -c "^.*" $output_dir/clog.false.positive.csv`
-    local clang_precision=`bc <<< "scale=0; $n_true_positive_clang/($n_true_positive_clang + $n_false_positive_clang) * 100"`
-    local clang_recall=`bc <<< "scale=0; $n_true_positive_clang/$n_ground * 100"`
-    local clog_precision=`bc <<< "scale=0; $n_true_positive_clog/($n_true_positive_clog + $n_false_positive_clog) * 100"`
-    local clog_recall=`bc <<< "scale=0; $n_true_positive_clog/$n_ground * 100"`
+    local clang_precision=`bc <<< "scale=2; 100.0 * $n_true_positive_clang/($n_true_positive_clang + $n_false_positive_clang)"`
+    local clang_recall=`bc <<< "scale=2; 100.0 * $n_true_positive_clang/$n_ground"`
+    local clog_precision=`bc <<< "scale=2; 100.0 * $n_true_positive_clog/($n_true_positive_clog + $n_false_positive_clog)"`
+    local clog_recall=`bc <<< "scale=2; 100.0 * $n_true_positive_clog/$n_ground"`
     local clang_time=`cat $output_dir/clang.analysis.time`
     local clang_time_f=`printf "%.2f" $clang_time`
     local clog_time=`cat $output_dir/clog.analysis.time`
     local clog_time_f=`printf "%.2f" $clog_time`
 
-    echo "$name & $clang_args & $n_ground & $n_true_positive_clang & $n_true_positive_clog & $clang_recall & $clog_recall & $n_false_positive_clang & $n_false_positive_clog & $clang_precision & $clog_precision & $clang_time_f & $clog_time_f \\tabularnewline" >> $file
+    echo "$name & $clang_args & $n_ground & $n_true_positive_clang & $n_true_positive_clog &  $n_false_positive_clang & $n_false_positive_clog & $clang_precision & $clog_precision & $clang_recall & $clog_recall & $clang_time_f & $clog_time_f \\tabularnewline" >> $file
+
 }
 
 RESULT_FILE=juliet-results.tex
